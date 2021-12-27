d2b9720813221faa40dee38547ab9ad37303f09a
Juan Ocho <juaneme8@gmail.com>
2021-12-27 14:42:15 -0300

    fetch using api function

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import './App.css'
+import api from './api'
+import { useEffect, useState } from 'react'
+import { Gift } from "./types";
+
+function App() {
+  const [gifts, setGifts] = useState<Gift[]>([])
+  
+  useEffect(() => {
+    api.gifts.list().then(gifts=> setGifts(gifts))
+  }, [])
+
+
+  console.log(gifts)
+
+  return (
+    <>
+      {gifts.length && gifts.map(gift => (
+        <div key={gift.id}>
+          <img src={gift.image} alt={gift.text} />
+          <p>{gift.text}</p>
+          <p>{gift.owner}</p>
+          <p>{gift.count}</p>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default App
---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export interface Gift {
+  id: number;
+  text: string;
+  owner: string;
+  image: string;
+  count: number;
+}
